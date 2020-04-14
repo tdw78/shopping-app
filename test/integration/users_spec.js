c5b8bce76ce0542ae4e30feff59648a863ba0aac
Tim Weiss <tdweiss1978@gmail.com>
2020-04-13 22:53:38 -0700

    Specs complete

--- test/integration/users_spec.js
+++ test/integration/users_spec.js
@@ -1,8 +1,69 @@
+// const server = require("../../app");
+// const request = require("supertest");
+// const mongoose = require("mongoose");
+// const mongoDB = "mongodb://localhost:27017/test_database";
+// const expect = require('chai');
+
+// mongoose.connect(mongoDB, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true
+// })
+// .then(() => console.log("DB connected"))
+// .catch((err) => console.log("DB CONNECTION ERROR: ", err))
+
+
+// describe("routes : users", () => {
+  
+//   before(() => {
+//     server.listen(3001)
+//   });
+
+//   after((done) => {
+//     mongoose.connection.close();
+//     done();
+//   });
+
+//   describe("POST /signup", () => {
+
+//     const options = {
+//       url:"http://localhost:5000/api/users/signup" ,
+//       user: {
+//         name: "Ethan",
+//         email: "user@example.com",
+//         password: "123456789"
+//       }
+//     }
+
+//     it("should signup new user", (done) => {
+//       request(server).post(options).expect(200);
+//       done();
+//     })   
+    
+//     // it("should not signup new user with invaid inputs", (done) => {
+//     //   const options = {
+//     //     url:"http://localhost:5000/api/users/signup" ,
+//     //     user: {
+//     //       name: "Ethan",
+//     //       email: "ethan",
+//     //       password: "123456789"
+//     //     }
+//     //   }
+//     //   request(server).post(options).
+//     //      expect(err).to.not.be.null;
+//     //      done();
+//     // })  
+    
+//   })
+
+// })
+
+
 const server = require("../../app");
 const request = require("supertest");
 const mongoose = require("mongoose");
 const mongoDB = "mongodb://localhost:27017/test_database";
-const expect = require('chai');
+const User = require("../../models/User");
 
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
@@ -12,86 +73,47 @@ mongoose.connect(mongoDB, {
 .then(() => console.log("DB connected"))
 .catch((err) => console.log("DB CONNECTION ERROR: ", err))
 
+describe("routes : items", () => {
 
-describe("routes : users", () => {
-  
   before(() => {
     server.listen(3001)
   });
 
   after((done) => {
     mongoose.connection.close();
-    
-  });
+    done();
+  })
 
   describe("POST /signup", () => {
 
     const options = {
-      url:"http://localhost:5000/api/users/signup" ,
+      url:"http://localhost:5000/api/users/signup",
       user: {
-        name: "Ethan",
-        email: "user@example.com",
-        password: "123456789"
+        name: "Jerry",
+        email: "jerry@mail.com",
+        password: "1234abc"
       }
     }
 
-    it("should signup new user", (done) => {
+    it("should sign up a new user", (done) => {
       request(server).post(options).expect(200);
       done();
-    })   
-    
-    it("should not signup new user with invaid inputs", (done) => {
-      const options = {
-        url:"http://localhost:5000/api/users/signup" ,
-        user: {
-          name: "Ethan",
-          email: "ethan",
-          password: "123456789"
-        }
-      }
-      request(server).post(options).
-         expect(err).to.not.be.null;
-         done();
-    })  
-    
+    })                                     
   })
+  
+  it("should not create a new user with invalid password", (done) => {
+    
+    const options = {
+      url:"http://localhost:5000/api/users/signup",
+      user: {
+        name: "Jerry",
+        email: "jerry",
+        password: "1234abc"
+      }
+    }
+    request(server).post(options).expect(400);
+      done();
+  });
 
+  
 })
-
-
-// const mongoose = require("mongoose");
-// const mongoDB = "mongodb://localhost:27017/test_database";
-// mongoose.connect(mongoDB)
-// const User = require('../../models/User');
-
-// describe("User", () => {
-
-//   // beforeAll(async () => {
-//   //   await User.remove({});
-//   // })
-
-//   // afterEach(async () => {
-//   //   await User.remove({});
-//   // })
-
-//   // afterAll(async () => {
-//   //   await mongoose.connect.close();
-//   // })
-
-//   describe("save a new user", () => {
-//     it("saves a user", async () => {
-//       const user = new User({
-//         name: "Ethan",
-//         email: "ethan@mail.com",
-//         password: "1234abc"
-//       })
-
-//      const savedUser = await user.save();
-//      const expected = "Ethan";
-//      const actual = savedUser.name;
-//      expected(actual).toEqual(expected);
-
-//     })
-//   })
-
-// })
--- test/unit/item_spec.js
+++ test/unit/item_spec.js
@@ -1,93 +1,34 @@
-// const User = require("../../models/User");
-// const Item = require("../../models/Item");
-// const expect = require('chai');
-
-// describe("Item", () => {
-//   describe("#create()", () => {
- 
-//     it("should create a new item", (done) => {
-
-//        const user = {
-//          name: "Jonny",
-//          email: "jonny@mail.com",
-//          password: 'golfer'
-//        }
-//         User.create(user)
-//         const item = {
-//           name: "Cereal",
-//           quantity: 1,
-//           userId: user.id
-//         }
-
-//       Item.create(item, (err, item) => {
-//         expect(err).to.be.null;
-//         expect(item).to.be.an("object");
-//         expect(item.name).to.equal('Cereal');
-//       })
-//       done();
-//       });
-
-//       it("should not create a new item that does not meet validation requiremetns", (done) => {
-
-//         const user = {
-//           name: "Jonny",
-//           email: "jonny@mail.com",
-//           password: 'golfer'
-//         }
-//          User.create(user)
-//          const item = {
-           
-//            quantity: 1,
-//            userId: user.id
-//          }
- 
-//        Item.create(item)
-//        .then(item => {
-
-//        })
-//         .catch((err) => {
-//            expect(err).to.be.null;
-           
-        
-//        })
-//        done();
-//       })
-
-//   });
-// });
-
-
-
 const User = require("../../models/User");
 const Item = require("../../models/Item");
-const assert = require('chai');
+const assert = require('chai').assert;
 
 describe("Item", () => {
   describe("#create()", () => {
- 
     it("should create a new item", (done) => {
 
-       const user = {
-         name: "Jonny",
-         email: "jonny@mail.com",
-         password: 'golfer'
+      const user = {
+        name: "Jonny",
+        email: "jonny@mail.com",
+        password: 'golfer'
+      }
+       User.create(user)
+       const item = {
+         name: "Bread",
+         quantity: 1,
+         userId: user.id
        }
-        User.create(user)
-        const item = {
-          name: "Cereal",
-          quantity: 1,
-          userId: user.id
-        }
 
-      Item.create(item, (err, item) => {
-        assert.isNull(err);
-        assert.isObject(item);
-        assert.isDefined(item.name)
-      })
-      done();
-      });
-
-      it("should not create a new item that does not meet validation requiremetns", (done) => {
+     Item.create(item)
+       .then(item => {
+         assert.typeOf(item, 'object')
+     })
+       .catch((err) => { 
+        console.log(err)
+     })
+     done();
+    })
+        
+    it("should not create an item that does not meet validation requiremetns", (done) => {
 
         const user = {
           name: "Jonny",
